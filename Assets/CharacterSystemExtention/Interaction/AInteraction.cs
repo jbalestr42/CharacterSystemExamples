da74891b150574df905e74af4ac9a8892802c395
Julien Balestrieri <julien.balestrieri@gmail.com>
2020-02-19 14:47:33 +0100

    Add interaction system

---
+++ Assets/CharacterSystemExtention/Interaction/AInteraction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class AInteraction
+{
+    public abstract int GetLayer();
+    public virtual void OnMouseClick(Vector3 position) { }
+    public virtual void OnMouseEnter(Vector3 position) { }
+    public virtual void OnMouseOver(Vector3 position) { }
+    public virtual void OnMouseExit() { }
+    public virtual void Cancel() { }
+    public virtual void End() { }
+}
---
+++ Assets/CharacterSystemExtention/Interaction/CharacterInteraction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CharacterInteraction : AInteraction
+{
+    public CharacterInteraction()
+    {
+    }
+
+    public override int GetLayer()
+    {
+        return Layers.UI;
+    }
+
+    public override void OnMouseClick(Vector3 position)
+    {
+        InteractionManager.Instance.EndInteraction();
+    }
+
+    public override void OnMouseOver(Vector3 position)
+    {
+    }
+
+    public override void Cancel()
+    {
+    }
+
+    public override void End()
+    {
+    }
+}
